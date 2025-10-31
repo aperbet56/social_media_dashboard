cfc5e13156726e4de5fe9417f4db32e82365bbb7
Annabelle Perbet <annabelleperbet@yahoo.fr>
2025-10-31 18:35:15 +0100

    Amélioration des commentaires js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Récupération de l'élément input de type checkbox
 const checkbox = document.querySelector("input[name=theme]");
 
-// Ecoute de l'événemant change sur l'input de type checkbox
+// Ecoute de l'événemant "change" sur l'input de type checkbox
 checkbox.addEventListener("change", function () {
   if (this.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
